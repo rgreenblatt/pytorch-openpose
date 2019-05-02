82872b6f8c41be51222275996058c90b74eb2fac
rgreenblatt <greenblattryan@gmail.com>
2019-05-01 22:56:57 -0400

    Bug fixes

--- python/body.py
+++ python/body.py
@@ -115,6 +115,8 @@ class Body(object):
                     for j in range(nB):
                         vec = np.subtract(candB[j][:2], candA[i][:2])
                         norm = math.sqrt(vec[0] * vec[0] + vec[1] * vec[1])
+                        if norm == 0:
+                            continue
                         vec = np.divide(vec, norm)
 
                         startend = list(zip(np.linspace(candA[i][0], candB[j][0], num=mid_num), \
